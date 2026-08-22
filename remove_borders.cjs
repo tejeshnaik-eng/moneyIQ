1d1c844c3567a28f46d5935bab91e27bb5891656
tirumal naik <naiktirumal888@gmail.com>
2026-08-22 17:24:19 +0530

    chore: remove stale cjs/txt files and force update final mIQ logo

--- remove_borders.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/components/modules/PortfolioAnalyzer.tsx', 'utf8');
-
-// Remove border border-[#222] from all instances of bg-[#161616] p-6 rounded-xl
-content = content.replace(/className="bg-\[\#161616\] border border-\[\#222\] p-6 rounded-xl"/g, 'className="bg-[#161616] p-6 rounded-xl"');
-
-fs.writeFileSync('src/components/modules/PortfolioAnalyzer.tsx', content);
